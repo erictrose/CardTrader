3abe02610b091867a22f690a4a615c508919b644
gitlabadmin <erictrose@gmail.com>
2016-01-12 16:02:46 -0500

    added uglify to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,20 @@
-module.exports = function(grunt) {
+module.exports = function(grunt){
   // Do grunt-related things in here
+    
+    // Project configuration.
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      
+        uglify:{
+            my_target:{
+                files:{
+                    'public/<%= pkg.name %>.min.js' : ['public/<%= pkg.name %>.js']
+                }
+            }
+        }
+        
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
 };

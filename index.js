60655492205bbad2daabc53a058043ae346054d8
gitlabadmin <erictrose@gmail.com>
2016-01-12 14:30:26 -0500

    added bower components to the express stuff in index.ejs

--- index.js
+++ index.js
@@ -4,10 +4,13 @@
 var express = require('express'),
     app = express();
 
+//set view engine
+app.set('view engine', 'ejs');
+
 //set directories
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 //serve index ejs
 app.get('/', function(request, response){

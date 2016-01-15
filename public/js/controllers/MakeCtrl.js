834ff02bd1b6441a0e3ca8a56cddba52757b4c6e
gitlabadmin <erictrose@gmail.com>
2016-01-14 22:18:16 -0500

    limited highest rated cards to 8

--- public/js/controllers/MakeCtrl.js
+++ public/js/controllers/MakeCtrl.js
@@ -1,10 +1,10 @@
 app.controller("MakeCtrl", ["currentAuth", "$scope", "Cards", function(currentAuth, $scope, Cards) {
-    
+
     //get cards from factory
     $scope.cards = Cards;
  
-    //
+    //amount of new cards to show
     $scope.newAmount = '20';
+    $scope.ratedAmount = '8';
     
-
 }]);

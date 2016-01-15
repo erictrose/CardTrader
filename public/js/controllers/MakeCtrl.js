e7d52a1c654b759c1244c0298868b55abb8cc455
gitlabadmin <erictrose@gmail.com>
2016-01-15 15:18:23 -0500

    about to add 500 test cartds

--- public/js/controllers/MakeCtrl.js
+++ public/js/controllers/MakeCtrl.js
@@ -7,4 +7,13 @@ app.controller("MakeCtrl", ["currentAuth", "$scope", "Cards", function(currentAu
     $scope.newAmount = '20';
     $scope.ratedAmount = '8';
     
+    var pushArray = [1,2,3,4,5];
+    
+    //dev: add cards to database
+    $scope.addcards = function(){
+        for(i=0;i<10;i++){
+            console.log(pushArray[i]);
+        }
+    };
+    
 }]);

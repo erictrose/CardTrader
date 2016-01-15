ecc387c7a998a4077f1b099b87c0c506b367c96b
gitlabadmin <erictrose@gmail.com>
2016-01-14 19:11:28 -0500

    moved getting firebasearray from make controller to a factory

--- public/js/controllers/MakeCtrl.js
+++ public/js/controllers/MakeCtrl.js
@@ -1,21 +1,6 @@
-app.controller("MakeCtrl", ["currentAuth", "$scope", "$firebaseArray", function(currentAuth, $scope, $firebaseArray) {
-  // currentAuth (provided by resolve) will contain the
-  // authenticated user or null if not logged in
-    $scope.test = 'make test';
-    $scope.twentycards = ['x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x',];
-    $scope.tencards = ['x','x','x','x','x','x','x','x','x','x',];
-    $scope.fourcards = ['x','x','x','x',];
+app.controller("MakeCtrl", ["currentAuth", "$scope", "Cards", function(currentAuth, $scope, Cards) {
     
-    
-    
-    
-    
-    
-var firebaseCards = new Firebase("https://card-trader.firebaseio.com/cards");
-$scope.cards = $firebaseArray(firebaseCards);
-    
-    
-    
-    
-    
+    //get cards from factory
+    $scope.cards = Cards;
+ 
 }]);

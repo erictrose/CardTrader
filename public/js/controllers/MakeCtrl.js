bcdead83c42f1e6bf53fa950dabd36de22fb3cfa
gitlabadmin <erictrose@gmail.com>
2016-01-15 15:49:27 -0500

    added test cards to database

--- public/js/controllers/MakeCtrl.js
+++ public/js/controllers/MakeCtrl.js
@@ -7,13 +7,26 @@ app.controller("MakeCtrl", ["currentAuth", "$scope", "Cards", function(currentAu
     $scope.newAmount = '20';
     $scope.ratedAmount = '8';
     
-    var pushArray = [1,2,3,4,5];
+    var pushArray = [{},{},{}]
+        
+    
     
     //dev: add cards to database
-    $scope.addcards = function(){
-        for(i=0;i<10;i++){
-            console.log(pushArray[i]);
-        }
-    };
+    $scope.addCard = function(){
+        console.log('this will add the card');
+//        $scope.cards.$add({
+//            cardName: pushArray[i].cardName,
+//            cardAttack: pushArray[i].cardAttack,
+//            cardDefense: pushArray[i].cardDefense,
+//            cardTotal: pushArray[i].cardTotal,
+//            cardColor: pushArray[i].cardColor,
+//            cardType: pushArray[i].cardType,
+//            cardImg: pushArray[i].cardImg,
+//            cardDesc: pushArray[i].cardDesc,
+//            createTime: pushArray[i].createTime
+//            });
+        };
+    
+    
     
 }]);

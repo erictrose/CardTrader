98489920c7b08bcb8c1cb57d9d874c079186341a
gitlabadmin <erictrose@gmail.com>
2016-01-18 16:26:18 -0500

    profiles now getting basic info from all providers

--- public/js/controllers/ProfileCtrl.js
+++ public/js/controllers/ProfileCtrl.js
@@ -2,5 +2,37 @@ app.controller("ProfileCtrl", ["currentAuth", "$scope", function(currentAuth, $s
   // currentAuth (provided by resolve) will contain the
   // authenticated user or null if not logged in
     $scope.test = 'battle test';
+    
+    //define auth
     $scope.auth = currentAuth;
+    
+    //if logged in with github
+    if($scope.auth.auth.provider === 'github'){
+        $scope.auth1 = $scope.auth.github.username;
+        $scope.auth2 =$scope.auth.github.profileImageURL;
+        $scope.auth3 = 'cards';
+    };
+    
+    //
+    if($scope.auth.auth.provider === 'twitter'){
+        $scope.auth1 = $scope.auth.twitter.username;
+        $scope.auth2 =$scope.auth.twitter.profileImageURL;
+        $scope.auth3 = 'cards';
+    };
+    
+    //
+    if($scope.auth.auth.provider === 'google'){
+        $scope.auth1 = $scope.auth.google.displayName;
+        $scope.auth2 =$scope.auth.google.profileImageURL;
+        $scope.auth3 = 'cards';
+    };
+    
+    //
+    if($scope.auth.auth.provider === 'password'){
+//        $scope.auth1 = $scope.auth.password.username;
+        $scope.auth1 = $scope.auth.password.email;
+        $scope.auth2 =$scope.auth.password.profileImageURL;
+        $scope.auth3 = 'cards';
+    };
+    
 }]);

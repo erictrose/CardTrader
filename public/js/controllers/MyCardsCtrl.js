62e658d212465700e8dc81b5dd3228c528c5a997
gitlabadmin <erictrose@gmail.com>
2016-01-19 16:49:34 -0500

    clicking a card in the mycard section will now bring an editable modal up

--- public/js/controllers/MyCardsCtrl.js
+++ public/js/controllers/MyCardsCtrl.js
@@ -23,4 +23,52 @@ app.controller("MyCardsCtrl", ["currentAuth", "$scope", "$firebaseArray", functi
         window.history.back();
     }
     
+    //add modal switch
+    $scope.addModalOpen = false;
+    
+    //view modal switch
+    $scope.viewModalOpen = false;
+    
+//    //switch view modal function
+//    $scope.switchViewModal = function(card){
+//        //if modal is open, close it, vice verca
+//        if(!$scope.viewModalOpen){$scope.viewModalOpen=true}
+//        else{$scope.viewModalOpen=false};
+//        $scope.viewColor = card.cardColor;
+//        $scope.viewTitle = card.cardName;
+//        $scope.viewType = card.cardType;
+//        $scope.viewAttack = card.cardAttack;
+//        $scope.viewDefense = card.cardDefense;
+//        $scope.viewImg = card.cardImg;
+//        $scope.viewDesc = card.cardDesc;
+//        console.log(card);
+//        console.log('toggled view modal');
+//    };
+    
+    //switch add modal function
+    $scope.switchAddModal = function(card){
+        //if modal is open, close it, vice verca
+        if(!$scope.addModalOpen){$scope.addModalOpen=true}
+        else{$scope.addModalOpen=false};
+        console.log('toggled modal');
+        
+        //
+        $scope.myColor = card.cardColor;
+        $scope.myTitle = card.cardName;
+        $scope.myAttack = card.cardAttack;
+        $scope.myDefense = card.cardDefense;
+        $scope.myType = card.cardType;
+        $scope.myDesc = card.cardDesc;
+        $scope.myImg = card.cardImg;
+//        $scope.notify = 'click';
+        //
+//        $scope.files = '';
+    };
+    
+    
+    
+    
+    
+    
+    
 }]);

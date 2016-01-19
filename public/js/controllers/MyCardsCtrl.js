7e3f77a64e2895cdc47fae35f335973474a17104
gitlabadmin <erictrose@gmail.com>
2016-01-19 17:33:17 -0500

    clicking a card now bring up edit modal with card info

--- public/js/controllers/MyCardsCtrl.js
+++ public/js/controllers/MyCardsCtrl.js
@@ -1,68 +1,114 @@
 app.controller("MyCardsCtrl", ["currentAuth", "$scope", "$firebaseArray", function(currentAuth, $scope, $firebaseArray) {
   // currentAuth (provided by resolve) will contain the
   // authenticated user or null if not logged in
-    $scope.test = 'battle test';
+
     
 //    //define auth
 //    $scope.auth = currentAuth;
     
     //
-    $scope.switchViewModal = function(card){console.log('switchViewModal fired')};
+    $scope.notify = "";
+    
+    //defaults
+    $scope.myColor = '';
+    $scope.myTitle = '';
+    $scope.myAttack = '';
+    $scope.myDefense = '';
+    $scope.myType = '';
+    $scope.myDesc = '';
+    $scope.myImg = '';
+    $scope.currentCard = '';    
+
     
     //connect to firebase directories
     var myCardsRef = new Firebase("https://card-trader.firebaseio.com/cards/" + currentAuth.uid);
     
-    //link to variables
-    var myCards = $firebaseArray(myCardsRef);
+//    //link to variables
+//    var myCards = $firebaseArray(myCardsRef);
     
     //link to scope
-    $scope.myCards = myCards;
+    $scope.myCards = $firebaseArray(myCardsRef);
     
     //go back, needs to be a factory or service or something
     $scope.goBack = function(){
         window.history.back();
-    }
+    };
     
     //add modal switch
     $scope.addModalOpen = false;
     
-    //view modal switch
-    $scope.viewModalOpen = false;
-    
-//    //switch view modal function
-//    $scope.switchViewModal = function(card){
-//        //if modal is open, close it, vice verca
-//        if(!$scope.viewModalOpen){$scope.viewModalOpen=true}
-//        else{$scope.viewModalOpen=false};
-//        $scope.viewColor = card.cardColor;
-//        $scope.viewTitle = card.cardName;
-//        $scope.viewType = card.cardType;
-//        $scope.viewAttack = card.cardAttack;
-//        $scope.viewDefense = card.cardDefense;
-//        $scope.viewImg = card.cardImg;
-//        $scope.viewDesc = card.cardDesc;
-//        console.log(card);
-//        console.log('toggled view modal');
-//    };
     
     //switch add modal function
     $scope.switchAddModal = function(card){
         //if modal is open, close it, vice verca
-        if(!$scope.addModalOpen){$scope.addModalOpen=true}
-        else{$scope.addModalOpen=false};
-        console.log('toggled modal');
+        if(!$scope.addModalOpen){
+            console.log('toggled modal on');
+            $scope.addModalOpen=true;
+            
+            console.log(
+            
+            $scope.myColor,
+            $scope.myTitle,
+            $scope.myAttack,
+            $scope.myDefense,
+            $scope.myType,
+            $scope.myDesc,
+            $scope.myImg,
+            $scope.currentCard
+            
+            );
+            
+            console.log(
+            
+            card.cardColor,
+            card.cardName,
+            card.cardAttack,
+            card.cardDefense,
+            card.cardType,
+            card.cardDesc,
+            card.cardImg,
+            card
+            
+            );
+            
+
+            
+            $scope.myColor = card.cardColor;
+            $scope.myTitle = card.cardName;
+            $scope.myAttack = card.cardAttack;
+            $scope.myDefense = card.cardDefense;
+            $scope.myType = card.cardType;
+            $scope.myDesc = card.cardDesc;
+            $scope.myImg = card.cardImg;
+            $scope.currentCard = card;
+        }
+        else
+        {
+            console.log('toggled modal off');
+            $scope.addModalOpen=false};
+    
+//            $scope.myColor = '';
+//            $scope.myTitle = '';
+//            $scope.myAttack = '';
+//            $scope.myDefense = '';
+//            $scope.myType = '';
+//            $scope.myDesc = '';
+//            $scope.myImg = '';
+//            $scope.currentCard = '';
+    };
+    
+    
+    
+    //update function
+    $scope.updateCard = function(card){
         
-        //
-        $scope.myColor = card.cardColor;
-        $scope.myTitle = card.cardName;
-        $scope.myAttack = card.cardAttack;
-        $scope.myDefense = card.cardDefense;
-        $scope.myType = card.cardType;
-        $scope.myDesc = card.cardDesc;
-        $scope.myImg = card.cardImg;
-//        $scope.notify = 'click';
-        //
-//        $scope.files = '';
+        
+        console.log($scope.myTitle);
+        console.log($scope.myCards);
+        console.log($scope.currentCard);
+        
+//        myCards.$save($scope.currentCard);
+        
     };
     
     
